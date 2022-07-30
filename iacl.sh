a9f228193c59b60e097772d1f8f7d78bb6538945
renawmontanari <regiangelica@gmail.com>
2022-07-30 14:35:11 -0300

    ScriptIac

---
+++ iacl.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "Criando diretórios..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando grupos de usuários..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuários..."
+
+useradd carlos -c "Carlos da Silva" -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -c "Maria Barbosa" -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao -c "João Silva e Silva" -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+
+useradd debora -c "Debora Martins" -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G  GRP_VEN
+useradd sebastiana -c "Sebastiana Wolfen" -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd roberto -c "Roberto Mattos" -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+
+useradd josefina -c "Josefina Moraes" -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd amanda -c "Amanda Wilbert" -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd rogerio -c "Rogério Vasconcelos" -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+
+echo "Especificando permissões dos diretórios..."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Script executado com sucesso!"
+echo "Finalizando..."
+echo "Tudo pronto!"
+
